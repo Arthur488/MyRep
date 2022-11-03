2a7b81590091eaff69690113beb3ec6c7458f91e
Arthur <x4playerx4@ukr.net>
2022-11-03 16:35:00 +0200

    Enum

--- src/Scanner/Scanner1.java
+++ src/Scanner/Scanner1.java
@@ -1,4 +1,19 @@
 package Scanner;
 
+import java.util.Scanner;
+
 public class Scanner1 {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Input number: ");
+//        int number = scanner.nextInt();
+//        System.out.println(number);
+        Scanner scanner = new Scanner("Hello my friend!\n Kak dela?");
+
+        while (scanner.hasNextLine()){
+            System.out.println(scanner.nextLine());
+        }
+
+
+    }
 }
