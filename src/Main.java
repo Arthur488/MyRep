1beab8afe124afe9b68184634f398563919a9bfc
Arthur <x4playerx4@ukr.net>
2022-11-03 22:20:01 +0200

    Enum

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.lang.Math;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("advsv");
+        int b = 2;
+         double b2 = Math.sqrt(b);
+        //double b = (x + n / x) / 2;;
+        System.out.println(b2);
     }
 }
