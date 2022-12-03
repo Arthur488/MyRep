5e5174805e094cbfda9fae5541e5120234b896e7
Arthur <x4playerx4@ukr.net>
2022-12-04 00:45:09 +0200

    JUnit

---
+++ src/Unit_Tests/SimpleCalculator.java
@@ -0,0 +1,7 @@
+package Unit_Tests;
+
+public class SimpleCalculator {
+    public static int odd(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/Unit_Tests/SimpleCalculatorTest.java
@@ -0,0 +1,17 @@
+package Unit_Tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleCalculatorTest {
+    @Test
+    void test1(){
+        assertTrue(SimpleCalculator.odd(4, 10) > 0);
+    }
+
+    @Test
+    void test2(){
+        assertTrue(SimpleCalculator.odd(4, -10) > 0);
+    }
+}
